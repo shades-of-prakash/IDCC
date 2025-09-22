cefab84174bece8d862603c24f0d76f895e94f9f
idcc.rvrjc <idcc.rvrjc@gmail.com>
2025-09-22 12:06:20 +0000

    added components

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # IDCC
-# IDCC
--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { UserloginComponent } from './pages/userlogin/userlogin.component';
+import { MainLayoutComponent } from './layouts/main-layout/main-layout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:"",component:MainLayoutComponent,
+    },
+    {
+        path:"user-login",component:UserloginComponent,
+    }
+];
